e4c4b485f8e3fd4a2f46f326d05029e48fbf5bf7
ahmadniaz <ahmadniazpk123@gmail.com>
2025-03-05 16:24:00 +0200

    fixed a bug on dashboard home

--- src/pages/dashboard/components/homeSection/homeSecton.js
+++ src/pages/dashboard/components/homeSection/homeSecton.js
@@ -120,7 +120,7 @@ const HomeSection = ({
         <Grid2 item size={{ xs: 12, sm: 6, md: 4 }}>
           <CreateActionCard
             text={`${translations.DASHBOARD.CREATE_EVENT}`}
-            onClick={() => setActiveForm("event")}
+            onClick={() => setActiveForm("events")}
           />
         </Grid2>
       </Grid2>
